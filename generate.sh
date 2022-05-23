482f6077d717aa2aac434b7987c6b4008ca372f9
Dmitrii Kilishek <mentalclear@gmail.com>
2022-05-22 21:01:44 -0400

    Add python compile command, gen python code

--- generate.sh
+++ generate.sh
@@ -11,6 +11,10 @@ protoc --go_out=. --go_opt=paths=source_relative \
     --go-grpc_out=. --go-grpc_opt=paths=source_relative \
     calculator/calculatorpb/calculator.proto
 
+# Generate Go code 
 protoc --go_out=. --go_opt=paths=source_relative \
 --go-grpc_out=. --go-grpc_opt=paths=source_relative \
 blog/blogpb/blog.proto
+
+# Generate Python code 
+protoc -I=blog/blogpb --python_out=blog/blogpb blog/blogpb/blog.proto
